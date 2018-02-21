9c882e4c92f775f240d6711dfa0ea791a3863f5a
Mikita Saladkevich <solodkevich@gmail.com>
2018-02-21 11:56:42 +0300

    sqlx test

--- main.go
+++ main.go
@@ -1,26 +1,19 @@
 package main
 
-/*
-
-db = docker
-user = docker@docker
-
-CREATE TABLE cities (id SERIAL PRIMARY KEY NOT NULL, name varchar(80), lat real);
-
-INSERT INTO cities(name, lat) VALUES ('San Francisco', '194.4');
-INSERT INTO cities(name, lat) VALUES ('Los Angeles', '220.4');
-INSERT INTO cities(name, lat) VALUES ('Paris', '80.4');
-
-*/
-
 import (
 	"fmt"
+	"log"
 
-	"database/sql"
-
-	"github.com/go-pg/pg"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
+
+const schema = `
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities (id SERIAL PRIMARY KEY NOT NULL, name varchar(80), lat real);
+INSERT INTO cities(name, lat) VALUES ('San Francisco', '194.4');
+INSERT INTO cities(name, lat) VALUES ('Los Angeles', '220.4');
+INSERT INTO cities(name, lat) VALUES ('Paris', '80.4');`
 
 type City struct {
 	Id   uint32
@@ -28,76 +21,58 @@ type City struct {
 	Lat  float32
 }
 
-func GetCitiesSQL(db *pg.DB) ([]City, error) {
-	var cities []City
-	_, err := db.Query(&cities, `SELECT * FROM cities`)
-	return cities, err
-}
-
-func GetCityByObject(db *pg.DB) (City, error) {
-	с := City{
-		Id: 2,
-	}
-	err := db.Select(&с)
-	return с, err
-}
-
-func GetCitiesClassic() {
-
-	connStr := "host=127.0.0.1 port=32772 user=docker password=docker dbname=docker sslmode=disable"
-	db, err := sql.Open("postgres", connStr) // driver name
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	rows, err := db.Query("select * from cities")
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	cities := []City{}
-
-	for rows.Next() {
-		c := City{}
-		err := rows.Scan(&c.Id, &c.Name, &c.Lat)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		cities = append(cities, c)
-	}
-	for _, c := range cities {
-		fmt.Println(c.Id, c.Name, c.Lat)
-	}
-
-}
-
 func main() {
 
-	db := pg.Connect(&pg.Options{
-		Addr:     "localhost:32772",
-		Database: "docker",
-		User:     "docker",
-		Password: "docker",
-	})
-	defer db.Close()
-
-	cities, err := GetCitiesSQL(db)
+	db, err := sqlx.Connect("postgres", "user=docker dbname=docker password=docker sslmode=disable")
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
+	}
+	db.MustExec(schema)
+
+	cities := []City{}
+	db.Select(&cities, "SELECT * FROM cities LIMIT 100")
+	//san, los := cities[0], cities[1]
+	//fmt.Println(san, los)
+
+	rows, err := db.Queryx("SELECT * FROM cities LIMIT 100")
+	var list []map[string]interface{}
+
+	for rows.Next() {
+		row := make(map[string]interface{})
+		err = rows.MapScan(row)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		list = append(list, row)
 	}
 
-	fmt.Println(cities)
+	fmt.Println(list[0]["id"], list[0]["name"], list[0]["lat"])
+	fmt.Println(list[1]["id"], list[1]["name"], list[1]["lat"])
+	fmt.Println(list[2]["id"], list[2]["name"], list[2]["lat"])
 
-	city, err := GetCityByObject(db)
-	if err != nil {
-		panic(err)
-	}
+	/*
+		db := pg.Connect(&pg.Options{
+			Addr:     "localhost:5432",
+			Database: "docker",
+			User:     "docker",
+			Password: "docker",
+		})
+		defer db.Close()
 
-	fmt.Println(city)
+		cities, err := GetCitiesSQL(db)
+		if err != nil {
+			panic(err)
+		}
 
-	GetCitiesClassic()
+		fmt.Println(cities)
+
+		city, err := GetCityByObject(db)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(city)
+		GetCitiesClassic()
+	*/
 
 }
